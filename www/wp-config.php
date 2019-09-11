22a9d92078994aa19f67e813c12da76273a569d1
Egor Pluzhnov <info@pluzhnov.ru>
2019-09-11 11:47:44 +0300

    plugins

--- www/wp-config.php
+++ www/wp-config.php
@@ -83,6 +83,7 @@ define('DOMAIN_CURRENT_SITE', 'pluzhnov.ru');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
+define( 'SUNRISE', 'on' );
 /* Это всё, дальше не редактируем. Успехов! */
 
 /** Абсолютный путь к директории WordPress. */
